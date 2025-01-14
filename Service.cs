f965cd7054a3a580c825afef453c0c44ba28e19d
Sidney Andrews <sidandrews@microsoft.com>
2025-01-14 14:17:44 -0500

    Initial commit

---
+++ Service.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Azure.Cosmos;
+
+public class Service
+{
+    public void GetInvalidClient()
+    {
+        string endpoint = "<example-endpoint>";
+
+        string key = "<example-key>";
+
+        CosmosClient client = new(endpoint, key);
+    }
+
+    public void GetValidClient()
+    {
+        string endpoint = "<example-endpoint>";
+
+        TokenCredential tokenCredential = new ChainedTokenCredential(
+            new ManagedIdentityCredential(),
+            new AzureCliCredential()
+        );
+
+        CosmosClient client = new(endpoint, tokenCredential);
+    }
+}
